e8e542ea683dec2a93f3453d82c5319d18b95a32
daniliuskosmaches <nasarspektrreas@gmail.com>
2025-10-29 09:57:19 +0500

    Proxy Server

---
+++ src/main/java/com/example/proxyserver1/ProxyServer1Application.java
@@ -0,0 +1,13 @@
+package com.example.proxyserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxyServer1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proxyserver1/ProxyServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proxyserver1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
